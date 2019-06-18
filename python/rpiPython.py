47786c01b09d80794c8b1c73e635116b4c836166
alemantus <alexander@anak.dk>
2019-06-18 11:15:16 +0100

    bme og interrupt

--- python/rpiPython.py
+++ python/rpiPython.py
@@ -4,7 +4,7 @@ from google.protobuf.json_format import MessageToJson
 logging.basicConfig(level=logging.INFO)
 
 ###
-
+import bme280
 import sys
 import time
 import ISO10368Lib
@@ -21,17 +21,22 @@ lowerBound = ""
 upperBound = ""
 setPoint = ""
 connectControl = 0
-kickControl = 2
+kickControl = 0
+ourCounter = 0
 
 tempList = [lowerBound,upperBound,setPoint,kickControl]
 
 tempContainer = -30
 
+#temp sensor
+bme = bme280.Bme280()
+bme.set_mode(bme280.MODE_FORCED)
+
 while(1):
 
     # mqtt initialize
     def on_connectDTU(client, userdata, flags, rc):
-        print("Test - connectDTU halo")  
+        #print("Test - connectDTU halo")  
         clientDTU.subscribe("EVBE/#")
 
     def on_messageDTU(client, userdata, msg):
@@ -40,11 +45,11 @@ while(1):
         data = UniversalFormat_pb2.Data()
         data.ParseFromString(msg.payload)
 
-        if(data.channel == "meter_current_phase_2"):
-            print('Value type: ' + data.WhichOneof('value'))
+        #if(data.channel == "meter_current_phase_2"):
+            #print('Value type: ' + data.WhichOneof('value'))
             
-            print(str(data.timestamp) + ' ' + data.meta['unit_name'] \
-                    + '(' + data.channel + '): ' + str(data.double) + ' ' + data.unit)
+            #print(str(data.timestamp) + ' ' + data.meta['unit_name'] \
+                   # + '(' + data.channel + '): ' + str(data.double) + ' ' + data.unit)
 
     def on_publishDTU(client,userdata,result):
         print("data published \n")
@@ -52,13 +57,15 @@ while(1):
 
     # mqtt initialize
     def on_connectOwn(client, userdata, flags, rc):  
-        # print("Test - connectOwn")      
+        print("Test - connectOwn")      
         clientOwn.subscribe("/container"+RCDid+"/lowerBound")
         clientOwn.subscribe("/container"+RCDid+"/upperBound")
         clientOwn.subscribe("/container"+RCDid+"/setPoint")
         clientOwn.subscribe("/container"+RCDid+"/kick")
 
-    def on_messageOwn(client, userdata, msg):    
+    def on_messageOwn(client, userdata, msg):   
+        global kickControl
+
         if(str(msg.topic) == "/container"+RCDid+"/lowerBound"):
             tempList[0] = int(msg.payload)
             tempList[3] = 0
@@ -94,7 +101,7 @@ while(1):
         clientOwn.on_connect = on_connectOwn
         clientOwn.on_message = on_messageOwn
 
-        #clientOwn.username_pw_set(username="admin",password="mqtt")
+        #clientOwn.username_pw_set(username="mqtt",password="3329646")
         #clientOwn.connect("87.63.168.126", 1883, 60)
 
         clientOwn.username_pw_set(username="jfccfsnl",password="a95j7N6GPemj")
@@ -107,17 +114,24 @@ while(1):
     Sair = ""
     Rair = ""
     global tempList
+    global ourCounter
 
-    if(tempList[0] != "" and tempList[1] != "" and tempList[2] != "" ):
-        # ensures that we only calculate new values on send from ui
-        if (tempList[3] == 1):
+
+    while(tempList[3] == 1 or ourCounter >= 5): 
+        t, p, h = bme.get_data()
+        print(round(t,2))
+        clientOwn.publish("/container"+RCDid+"/bmeTemp", Rair, qos=0, retain=False)
+        if(tempList[0] != "" and tempList[1] != "" and tempList[2] != "" ):
             bitString, Sair, Rair = ISO10368Lib.containerString(tempList[0], tempList[1], tempContainer)
-        if(bitString != "" and Sair != "" and Rair != ""):
-            clientOwn.publish("/container"+RCDid+"/bitString", bitString, qos=0, retain=False)
-            clientOwn.publish("/container"+RCDid+"/Sair", Sair, qos=0, retain=False)
-            clientOwn.publish("/container"+RCDid+"/Rair", Rair, qos=0, retain=False)
-            print(Sair)
+            if(bitString != "" and Sair != "" and Rair != ""):
+                clientOwn.publish("/container"+RCDid+"/bitString", bitString, qos=0, retain=False)
+                clientOwn.publish("/container"+RCDid+"/Sair", Sair, qos=0, retain=False)
+                clientOwn.publish("/container"+RCDid+"/Rair", Rair, qos=0, retain=False)
+                print(Sair)
+        tempList[3] = 0
+        ourCounter = 0
 
     clientDTU.loop_start()
     clientOwn.loop_start()
-    time.sleep(5)
+    ourCounter += 1
+    time.sleep(1)

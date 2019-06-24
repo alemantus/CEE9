b03fc011e7db5fd63de1d893f394760b9fbb441c
Alexander Mizrahi-Werner <alexander@anak.dk>
2019-06-24 09:57:36 +0200

    Update ISO10368Lib.py

--- python/ISO10368Lib.py
+++ python/ISO10368Lib.py
@@ -1,10 +1,15 @@
 import datetime
+from gpiozero import LED
 
 now = datetime.datetime.now()
 
+led18 = LED(17)
+led30 = LED(18)
+ledOp = LED(27)
+ledNed = LED(22)
 
-test = "hej"
-print(test)
+#test = "hej"
+#print(test)
 def ISOstring(temp):
     tempISOdouble = (125 + temp)/0.05
     tempISO = round(tempISOdouble)
@@ -40,6 +45,10 @@ def containerString(MstartCool, AstartCool, tempContainer, StartPeakshaving, Tes
         # LED skal lyse i stedet for printout 
         test1 = "Peak-shaving i gang - Containeren slukkes"
         print(test1)
+        led18.off()
+        led30.off()
+        ledNed.on()
+        ledOp.off()
     elif (tempContainer <= -30):
         tempDesired = -30
         Sair = -30
@@ -50,6 +59,10 @@ def containerString(MstartCool, AstartCool, tempContainer, StartPeakshaving, Tes
         # LED skal lyse i stedet for printout 
         test2 = "Containertemeraturen holdes på -30 grader"
         print(test2)
+        led18.off()
+        led30.on()
+        ledNed.off()
+        ledOp.off()
     elif (Startcool == 1):
         tempDesired = -30
         Sair = 75
@@ -60,6 +73,10 @@ def containerString(MstartCool, AstartCool, tempContainer, StartPeakshaving, Tes
         # LED skal lyse i stedet for printout 
         test3 = "Containerens setpoint ændres til -30 grader (Nedkølingstid 1 time)"
         print(test3)
+        led18.off()
+        led30.off()
+        ledNed.off()
+        ledOp.on()
     else :
         tempDesired = -20
         Sair = -25
@@ -69,7 +86,11 @@ def containerString(MstartCool, AstartCool, tempContainer, StartPeakshaving, Tes
         RairISO = "0" + ISOstring(Rair)
         # LED skal lyse i stedet for printout 
         test4 = "Containeren holdes på -18 grader (Normal tilstand)"
-        print(test4)   
+        print(test4)
+        led18.on()
+        led30.off()
+        ledNed.off()
+        ledOp.off()   
         
 
     # Alarm string, 100 angiver standard alarm
